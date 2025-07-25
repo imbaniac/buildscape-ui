c7e95b752be5cb640913378dcfad19f05aec0cb6
Tim Reznichenko <tim.reznich@gmail.com>
2025-07-25 22:42:53 +0700

    feat: search in url

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,3 +1,13 @@
+import type { PageLoad } from './$types';
+
 // Enable prerendering for the homepage
 export const prerender = true;
 export const ssr = true;
+
+export const load: PageLoad = ({ url }) => {
+  const searchQuery = url.searchParams.get('q') || '';
+  
+  return {
+    searchQuery
+  };
+};
